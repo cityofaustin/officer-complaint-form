b4cbd9ff428ddacdf2e372dba172a14b32d9bccb
Sergio Garcia <sergiogcx@gmail.com>
2019-01-29 13:59:37 -0600

    Added support for confirmation case number

--- js/config/form.js
+++ js/config/form.js
@@ -12,13 +12,62 @@ import {
 } from './chapters';
 
 const formConfig = {
+  type: "complaint",
+  language: "en",
   title: 'File a complaint',
   subTitle: '',
   formId: '',
   urlPrefix: '/',
   trackingPrefix: 'form-',
-  transformForSubmit: '',
-  submitUrl: '',
+  transformForSubmit: (formConfig, form) => {
+    form.data.language = formConfig.language;
+    form.data.type = formConfig.type;
+    return JSON.stringify(form.data);
+  },
+  submit: (formData, formConfig) => {
+    console.log("Submitting:");
+    console.log(formData.data);
+
+  	// Create the XHR request
+  	var request = new XMLHttpRequest();
+  	// Return it as a Promise
+  	return new Promise(function (resolve, reject) {
+  		// Setup our listener to process compeleted requests
+  		request.onreadystatechange = function () {
+  			// Only run if the request is complete
+  			if (request.readyState !== 4) return;
+  			// Process the response
+  			if (request.status >= 200 && request.status < 300) {
+  				// If successful
+  				resolve(request);
+  			} else {
+  				// If failed
+  				reject({
+  					status: request.status,
+  					statusText: request.statusText
+  				});
+  			}
+  		};
+  		// Setup our HTTP request, headers & send our payload ...
+  		request.open('POST', formConfig.submitUrl, true);
+  		request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  		request.send(JSON.stringify(formData.data));
+  	})
+    .then(function (response) {
+      console.log("Success: ");
+      console.log(response.response);
+      let respObj = JSON.parse(response.response);
+      console.log(respObj.case_number);
+      localStorage.setItem("opo_confirmation_case_number", respObj.case_number);
+      return response;
+  	})
+  	.catch(function (error) {
+      console.log("Error: ");
+      console.log(error);
+      return error;
+  	});
+  },
+  submitUrl: `${process.env.API_URL}/form/submit`,
   introduction: Introduction,
   confirmation: Confirmation,
   defaultDefinitions: {},
