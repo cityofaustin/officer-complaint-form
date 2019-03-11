f4fd303678629917647cb39d9bb791326297ff81
Sergio Garcia <sergiogcx@gmail.com>
2019-03-11 11:22:23 -0500

    Style fine-tuning

--- js/components/Confirmation.jsx
+++ js/components/Confirmation.jsx
@@ -9,13 +9,25 @@ class Confirmation extends React.Component {
     constructor(props) {
         super(props);
 
+        let formData = JSON.parse(localStorage.getItem("opo_form_data"));
+        let confirmationNumber = localStorage.getItem("opo_confirmation_case_number") || "N/A";
+        let submitUrl = localStorage.getItem("opo_form_submiturl") || "";
+        let userEmail = "";
+        try {
+            userEmail = formData["view:contactPreferences"].yourEmail;
+        } catch {
+            userEmail = "";
+        }
+
+        let initialSubmissionState = this.validateEmail(userEmail) ? -1 : 0;
+
         this.state = {
-            formSubmissionState: 0,
-            userEmail: "",
+            formSubmissionState: initialSubmissionState,
+            userEmail: userEmail,
             errorMessage: "",
-            formData: JSON.parse(localStorage.getItem("opo_form_data")),
-            submitUrl: localStorage.getItem("opo_form_submiturl"),
-            confirmationCaseNumber: (localStorage.getItem("opo_confirmation_case_number") || "N/A")
+            formData: formData,
+            submitUrl: submitUrl,
+            confirmationCaseNumber: confirmationNumber
         };
     }
 
@@ -24,25 +36,37 @@ class Confirmation extends React.Component {
         return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
     }
 
+
+
     // Submits the form
     resubmitForm() {
-        console.log("resubmitForm() We are going to output your data here: ");
-        console.log(this.state);
+        // console.log("resubmitForm() We are going to output your data here: ");
+        // console.log(this.state);
         let formData = this.state.formData;
 
         // Let's validate the email address, or show an error message ...
         if(this.validateEmail(this.state.userEmail) == false || this.state.userEmail == "") {
-            console.log("We have a problem with the email: " + this.state.userEmail);
+            // console.log("We have a problem with the email: " + this.state.userEmail);
             this.setState({
                 errorMessage: "Please provide a valid email address."
             });
         } else {
             // Let's change the form data prior to submission ...
-            let old_email = formData["view:contactPreferences"].yourEmail;
-            formData["view:contactPreferences"].yourEmail = this.state.userEmail;
-            let new_email = formData["view:contactPreferences"].yourEmail;
-            console.log("OldE: " + old_email);
-            console.log("NewW: " + new_email);
+            // console.log("resubmitForm() No email present, reassigning...");
+
+            // Try to reassign the email, or set up the value.
+            try {
+                // console.log("resubmitForm() view:contactPreferences['yourEmail'] : " + this.state.userEmail);
+                formData["view:contactPreferences"] = {
+                    "yourEmail": this.state.userEmail
+                };
+                console.log(formData);
+            } catch {
+                // console.log("Could not set up the email address.");
+                // console.log(formData);
+                return;
+            }
+
 
             // Append two needed fields for the backend...
             formData.userConfirmationOnly = true;
@@ -56,7 +80,7 @@ class Confirmation extends React.Component {
             // Run the loading (three dots) animation...
             this.runLoaderAnimation();
 
-            // setTimeout(this.markDone, 1000, this);
+            setTimeout(this.markDone, 1000, this);
 
             // Make the XHR request with the formData and send to the submitUrl endpoint
             this.make_xhr_request(formData, this.state.submitUrl, this);
@@ -84,11 +108,21 @@ class Confirmation extends React.Component {
         })
     }
 
+    // Clears all the keys used during the form...
+    clearLocalStorage() {
+        // console.log("clearLocalStorage() Clearing opo_form_submiturl");
+        localStorage.removeItem("opo_form_submiturl");
+        // console.log("clearLocalStorage() Clearing opo_confirmation_case_number");
+        localStorage.removeItem("opo_confirmation_case_number");
+        // console.log("clearLocalStorage() Clearing opo_form_data");
+        localStorage.removeItem("opo_form_data");
+    }
+
     // Instantiates an XHR request, then submits to submitUrl
     make_xhr_request(formData, submitUrl, formContext = null) {
-        console.log("API_URL: " + submitUrl);
-        console.log("Appending formConfig to formData!");
-        console.log(formData);
+        // console.log("API_URL: " + submitUrl);
+        // console.log("Appending formConfig to formData!");
+        // console.log(formData);
 
         // Create the XHR request
         var request = new XMLHttpRequest();
@@ -96,34 +130,30 @@ class Confirmation extends React.Component {
         return new Promise(function (resolve, reject) {
                 // Setup our listener to process complete requests
                 request.onreadystatechange = function () {
-                // Only run if the request is complete
-                if (request.readyState !== 4) return;
-                    // Process the response
-                if (request.status >= 200 && request.status < 300) {
-                    // If successful
-                    resolve(request);
-                } else {
-                    // If failed
-                    reject({
-                               status: request.status,
-                               statusText: request.statusText
-                           });
+                    // Only run if the request is complete
+                    if (request.readyState !== 4) return;
+                        // Process the response
+                    if (request.status >= 200 && request.status < 300) {
+                        // If successful
+                        resolve(request);
+                    } else {
+                        // If failed
+                        reject({
+                                   status: request.status,
+                                   statusText: request.statusText
+                               });
                     }
                 };
 
-                // Setup our HTTP request, headers & send our payload ...
-                request.open('POST', submitUrl, true);
-                request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+            // Setup our HTTP request, headers & send our payload ...
+            request.open('POST', submitUrl, true);
+            request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
 
-                request.send(JSON.stringify(formData));
+            request.send(JSON.stringify(formData));
             })
             .then(function (response) {
                 formContext.markDone(formContext);
-                console.log("Success: ");
-                console.log(response.response);
-                let respObj = JSON.parse(response.response);
-                console.log(respObj.case_number);
-                localStorage.setItem("opo_confirmation_case_number", respObj.case_number);
+                formContext.clearLocalStorage();
                 return response;
             })
             .catch(function (error) {
@@ -156,27 +186,35 @@ class Confirmation extends React.Component {
 
     render() {
         let confirmationCaseNumber = this.state.confirmationCaseNumber;
+        let confirmation_content = null;
 
-        let buttonContent = this.renderButton();
+        if(this.state.formSubmissionState === -1) {
+            console.log("renderConfirmation() The email looks valid, so no need to render form");
+            confirmation_content = <div style={{marginTop: "4rem"}}>
+                You will receive an email with a copy of your complaint and a confirmation number at <b>{this.state.userEmail}</b>
+            </div>;
+
+            this.clearLocalStorage();
+        } else {
+            let buttonContent = this.renderButton();
+            confirmation_content = <div className="confirmation">
+                <p style={{fontSize: "1.4rem"}}>For a copy of your complaint and confirmation number, enter your email address below.</p>
+                <span style={{fontSize: "2rem"}}>Your email</span>
+                <input className="confimation-input" type="email" disabled = {(this.state.formSubmissionState > 0) ? "disabled" : ""} value={this.state.userEmail} onChange={(event) => this.setState({userEmail: event.target.value})} />
+                {buttonContent}
+                <div className="confirmation-errorbox" style={(this.state.errorMessage.length === 0) ? {display: "none"} : {display: "block"}}>{this.state.errorMessage} (<a href="javascript:;" onClick={() => this.hideErrorMessage()}>dismiss</a>)</div>
+            </div>;
+        }
 
         return (
           <div className="schemaform-intro">
             <SegmentedProgressBar total={2} current={2}/>
-            <h2 style={{textAlign: "center"}}>We have received your complaint</h2>
-            <h3 style={{color: "#164ED2", textAlign: "center"}}>Your case number: {confirmationCaseNumber}</h3>
-            <div className="confirmation">
-                <hr/>
-                <p style={{fontSize: "1.4rem"}}>For a copy of your complaint and confirmation number, enter your email address below.</p>
-                <input className="confimation-input" type="email" disabled = {(this.state.formSubmissionState > 0) ? "disabled" : ""} value={this.state.userEmail} onChange={(event) => this.setState({userEmail: event.target.value})} />
-                {buttonContent}
-                <div className="confirmation-errorbox" style={(this.state.errorMessage.length === 0) ? {display: "none"} : {display: "block"}}>{this.state.errorMessage} (<a href="javascript:;" onClick={() => this.hideErrorMessage()}>dismiss</a>)</div>
-            </div>
-            <hr/>
-            <p style={{fontSize: "1.4rem"}}>Our job is to make sure your complaint is investigated fairly and thoroughly. Thank you for sharing your experience with us. This helps us better serve you and your community.</p>
-            <hr/>
-            <p style={{fontSize: "1.4rem"}}>You will receive an email with a copy of your complaint and a confirmation number. You can email us at <a>policeoversight@austintexas.gov</a> or call us at <a>512-972-2676</a> with your confirmation number to find where your complaint is in this process.</p>
+            <h2 style={{textAlign: "center", fontSize: "3rem", fontWeight: "Normal", width: "80%", marginLeft: "10%"}}>We have received your complaint.</h2>
+            <h3 style={{color: "#164ED2", textAlign: "center", fontSize: "1.8rem"}}>Your confirmation number: {confirmationCaseNumber}</h3>
+            {confirmation_content}
+            <p style={{fontSize: "1.4rem"}}>You can email us at <a>policeoversight@austintexas.gov</a> or call us at <a>(512) 972-2676</a> with your confirmation number to find where your complaint is in this process.</p>
             <p style={{fontSize: "1.4rem"}}>If you provided your contact information, a staff person from the Office of Police Oversight will contact you within two to four business days.</p>
-            <hr/>
+            <p style={{fontSize: "1.4rem"}}>Our job is to make sure your complaint is investigated fairly and thoroughly. Thank you for sharing your experience with us. This helps us better serve you and your community.</p>
             <p style={{fontSize: "1.4rem"}}><a href="http://alpha.austin.gov/police-oversight/complaint-investigation-process">What happens next</a></p>
           </div>
         );
